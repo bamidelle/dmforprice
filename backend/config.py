33b09b93b65e474b1cf38fd7eb47e203d983e4f5
bamidelle <48367095+bamidelle@users.noreply.github.com>
2026-02-25 17:14:26 +0100

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-class Settings:
-    SUPABASE_URL = st.secrets["SUPABASE_URL"]
-    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-    JWT_SECRET = st.secrets.get("JWT_SECRET", "dev-secret")
-
-
-
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+JWT_SECRET = st.secrets.get("JWT_SECRET", "dev-secret")
