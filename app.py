dcf6cf3f9aada5726b6aa95799b4b61a1a4ea4a6
bamidelle <48367095+bamidelle@users.noreply.github.com>
2026-02-25 15:59:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 #from backend.routers.auth import login_user, signup_user
 
+from backend.routers.auth import login_user, signup_user
+
 st.set_page_config(page_title="DM for Price")
 
 # -------------------------
